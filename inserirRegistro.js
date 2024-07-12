3be66f755467fc003aa4c51394b29e5fc7a411ba
marvencbss <130517545+marvencbss@users.noreply.github.com>
2024-07-11 21:16:32 -0300

    aula

---
+++ inserirRegistro.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require('mongodb');
+
+
+const url = 'mongodb://127.0.0.1:27017'; // Substitua pela URL do seu MongoDB
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const dbName = 'mongodb';
+
+async function main() {
+  await client.connect();
+  console.log('Conectado com sucesso ao servidor MongoDB');
+
+  const db = client.db(dbName);
+  const collection = db.collection('Cadastro');
+
+  const insertResult = await collection.insertOne({ nome: 'Davi', idade: 30, cidade: 'Porto Alegre' });
+  console.log('Documento inserido:', insertResult.insertedId);
+  
+  const findResult = await collection.find({}).toArray();
+  console.log('Documentos encontrados:', findResult);
+
+  await client.close();
+}
+
+main().catch(console.error);
